804d0636cedc666faf5a2444969551830b493f44
chak8x8 <chak8x8@gmail.com>
2025-06-14 14:35:36 +1000

    0. FizzBuzz

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -2,10 +2,11 @@
 def fizzbuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end=" ")
         elif i % 3 == 0:
-            print("Fizz")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("Buzz")
+            print("Buzz", end=" ")
         else:
-            print(i)
+            print(i, end=" ")
+    print()
