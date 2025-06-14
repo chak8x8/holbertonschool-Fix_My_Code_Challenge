f8e0c3686cc4dea5cc87176198cd9bcd09b3ad7d
chak8x8 <chak8x8@gmail.com>
2025-06-14 15:22:03 +1000

    2. Sort

---
+++ challenge/2-sort.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+    
+    # insert at the right position
+    i = 0
+    while i < result.size && result[i] < i_arg
+        i += 1
+    end
+    result.insert(i, i_arg)
+end
+
+puts result
